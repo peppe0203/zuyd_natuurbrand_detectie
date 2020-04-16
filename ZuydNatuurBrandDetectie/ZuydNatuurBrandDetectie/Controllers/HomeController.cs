9385c5576da9f8ffef1dc3a867776720dedb23ed
TKusters <Tonito.kusters@yahoo.com>
2020-04-16 16:55:00 +0200

    dashboard + nodeDashboard
    Functioneel  overzicht voor alle nodes

--- ZuydNatuurBrandDetectie/ZuydNatuurBrandDetectie/Controllers/HomeController.cs
+++ ZuydNatuurBrandDetectie/ZuydNatuurBrandDetectie/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ZuydNatuurBrandDetectie.Models;
@@ -53,6 +54,69 @@ namespace ZuydNatuurBrandDetectie.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            nodeTbl nodeTbl = db.nodeTbls.Find(id);
+            if (nodeTbl == null)
+            {
+                return HttpNotFound();
+            }
+
+         
+            var metingen = db.metingTbls.ToList();
+            var meldingen = db.meldingenTbls.ToList();
+            var koppeltbl = db.koppelmeldingTbls.ToList();
+            List<metingTbl> gevondenMetingen = new List<metingTbl>();
+            List<meldingenTbl> gevondenMeldingen = new List<meldingenTbl>();
+            List<int> koppeling = new List<int>();
+
+            foreach (var item in metingen)
+            {
+                if (item.nodeId == id)
+                {
+                    gevondenMetingen.Add(item);
+                }
+            }
+
+            foreach (var item in koppeltbl)
+            {
+                if (item.nodeId == id)
+                {
+                    koppeling.Add(item.meldingId);
+                }
+            }
+
+            foreach (var item in koppeling)
+            {
+                foreach (var melding in meldingen)
+                {
+                    if (item == melding.id)
+                    {
+                        gevondenMeldingen.Add(melding);
+                    }
+                }
+            }
+
+            NodeVm model = new NodeVm
+            {
+                node = nodeTbl,
+                metingen = gevondenMetingen,
+                meldingen = gevondenMeldingen
+            };
+
+
+
+
+
+
+
+            return View(model);
         }
 
     }
---
+++ ZuydNatuurBrandDetectie/ZuydNatuurBrandDetectie/Models/NodeVm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZuydNatuurBrandDetectie.Models
+{
+    public class NodeVm
+    {
+        public nodeTbl node { get; set; }
+        public List<metingTbl> metingen { get; set; }
+        public List<meldingenTbl> meldingen { get; set; }
+    }
+}
